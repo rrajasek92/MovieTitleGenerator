c07129533434300ac46addd96d1e38fd4d4ebd41
GBTC440012UR <GBTC440012UR@GBTC363I12W7.wdce.local>
2015-10-15 09:56:30 -0400

    first

---
+++ MovieTitleGenerator/src/MovieTitleGenerator/Movie.java
@@ -0,0 +1,12 @@
+package MovieTitleGenerator;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.Random;
+public class Movie 
+{
+	public static void main(String[] args) throws Exception
+	{
+		MovieTitleGenerator.generate();
+	}
+
+}
